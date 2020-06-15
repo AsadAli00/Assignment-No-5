25eeebbfb89de29a18e32715e08bc6555bfe9bf4
Asad ali <46523348+AsadAli00@users.noreply.github.com>
2020-06-15 22:42:13 +0500

    update app.js chapter no 5
    chapter no 5

--- app.js
+++ app.js
@@ -103,13 +103,150 @@ var Quantity=5;
 
 alert(name+"ordered"+Quantity+product+" on XYZ Clothing store");
 
+//chapter no 4
+//task no 1
+
+var name,age,rollno;
+
+//task no 2
+
+var name,my_name,my_1st_name,n2me;
+var alert,2name,my name, console,1111;
+
+//task no 3
+
+document.write("variable names can only contain, number, $ and ______, For Example :$my_1stVariable");
+document.write("variable must begin with a letter.$ or___.For Example:$name_name or name");
+document.write("variable name are case sensitive");
+document.write("variable name should note be JS Keyword");
 
 
 
+//chapter no 5 
+
+//task no 1
+
+var num1 =3;
+var num2 =5;
+var ans;
+
+document.write("Sum of "+num1+"and "+num2+" is "+ num1 + num2);
+
+
+//task no 2 
+
+var num1 =3;
+var num2 =5;
+var ans;
+
+document.write("Subtraction of "+num1+"and "+num2+" is "+ num1 - num2);
+document.write("Multiplication of "+num1+"and "+num2+" is "+ num1 * num2);
+document.write("Division of "+num1+"and "+num2+" is "+ num1 / num2);
+document.write("Modulus of "+num1+"and "+num2+" is "+ num1 % num2);
+
+//task no 3
+var val = 5;
+var valinc = val++;
+var add = val + 7;
+var dec = val--;
+
+document.write("value after variable decleration is undefined\n");
+var val = 5;
+document.write("Initial value :"+val+"\n");
+var val = val++;
+document.write("value after increment is:"+val+"\n");
+var val = val + 7;
+document.write("value after addition is :"+val+"\n");
+var val = val--;
+document.write("Value after decrement is :"+val+"\n");
+var val = val/3;
+document.write("The reminder is :"+val+"\n");
+
+
+//task no 4
+var val = 5;
+var  ticket = 600;
+var buy = ticket * val;
+document.write("Total cost to buy"+val+"tickets to a movie is "+3000+"PKR");
+
+
+//task no 5 
+var num = 4;
+document.write("Table of 4");
+for(var i=1;i<=10;i++){
+    document.write(num+"x"+i+"="+num*1+"\n");
+}
+
+//task no 6 
+
+var c =25;
+var c_to_f=(c*9/5)+32;
+document.write(c+"C is"+c_to_f+"F"+"\n");
+
+//task no 7
+
+ var item1 =100;
+ var item2=200;
+ var quantity1 = 3;
+ var quantity2 = 7;
+ var shipping = 100;
+
+ docunnet.write("Price of Item 1 is "+item1+"\n");
+ document.write("Quantity of item 1 is "+quantity1+"\n")
+ docunnet.write("Price of Item 2 is "+item2+"\n");
+ document.write("Quantity of item 2 is"+quantity2+"\n")
+ docunnet.write("Shipping Charges"+shipping+"\n\n");
+document.write("Total Cost of your order is"+(item1*quantity1)+(item2*quantity2)+shipping);
+
+//task no 8
+var total_mark = 1100;
+var obt_mark = 738;
+var per = obt_mark/total_mark*100;
+
+document.write("Total Mark: "+total_mark+"\n");
+document.write("Obtained Mark: "+obt_mark+"\n");
+document.write("Percentage:"+per+"\n");
 
 
 
+//task no 9
 
+document.write("Total Currancy in PKR:"+((10*104.80)+(25*28)));
 
+//task no 10
+var num = 5;
+var num1 = ((num+5)*10)/10;
+alert(num1);
+
+//task no 11
+
+var year = 2020;
+var birthyear= 1998;
+var age = year - birthyear;
+document.write("Current Year:"+year+"\n");
+document.write("Birth Year:"+birthyear+"\n");
+document.write("Your Age is:"+age+"\n");
+
+//task no 12
+const PI = 3.142;
+var radius = 20;
+var area = PI * (radius **2); 
+ var circum = 2 * PI * radius;
+ document.write("Radius Of Circle is:"+radius+"\n");
+ document.write("The CircumFerence is:"+circum+"\n");
+ document.write("The Area is:"+area+"\n");
+ 
+//task no 13 
+
+var snack = "Chocolate  chip";
+var age = 20;
+var maxage = 65;
+var per_day =3;
+var total = (65-20)*3*365;
+document.write("Favourite Snack:"+snack+"\n");
+document.write("Current age:"+age+"\n");
+document.write("Estimated Maximum Age:"+maxage+"\n");
+document.write("Ammount of  snack per day:"+per_day+"\n");
+document.write("You will need:"+total+"Chocolate  chip to last you until the ripe old age of "+maxage);
 
 

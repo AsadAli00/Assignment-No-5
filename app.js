a0d4d52eb189049975b1dfeca54efe633a24659a
Asad ali <46523348+AsadAli00@users.noreply.github.com>
2020-06-15 20:58:48 +0500

    chapter no 2

--- app.js
+++ app.js
@@ -26,6 +26,53 @@ console.log("Hello...I can JS through my web  browser's console");
 
 alert("Hello World");
 
+//chapter no 2
+
+//task no 1
+
+var username;
+
+//task no 2
+
+var myname = "Asad Ali";
+
+//task no 3
+
+var messaage = "Hello World";
+
+alert(messaage);
+
+
+//task no 4
+
+var name = "Asad ALi";
+var age = 22;
+var status ="Certified Mobile Application Development";
+
+alert(name);
+alert(age);
+alert(status);
+
+
+//task no 5
+
+var brand = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(brand);
+
+
+//task no 6
+
+var email = "example@example.com"
+alert("My email address is"+ email);
+
+//task no 7
+
+var book = "A Smarter way to learn JavaScript";
+alert("i am trying to learn from the book"+book);
+
+//task no 8
+
+document.write("Yah! I can write HTML content through JavaScript");
 
 
 
